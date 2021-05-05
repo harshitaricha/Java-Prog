a43e1e967b4693ee6a52ae8c1772df4ed5b65fee
Harshita Richa <49182423+harshitaricha@users.noreply.github.com>
2021-05-05 20:05:51 +0530

    Add files via upload

---
+++ Years.java
@@ -0,0 +1,13 @@
+class Week
+{
+	void display() {
+		System.out.println("DAYS");
+	}
+}
+class Years
+{
+	public static void main(String args[]) {
+		Week myWeek = new Week();
+		myWeek.display();
+	}
+}
